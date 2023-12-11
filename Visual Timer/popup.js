9a23ed375dcb586a58f9f97fbe06acae162084f8
cheny409 <cheny409@newschool.edu>
2023-12-10 19:06:37 -0500

    Create popup.js

---
+++ Visual Timer/popup.js
@@ -0,0 +1,5 @@
+document.getElementById('badge').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'createParallelogram' });
+  });
+});
