47b8cb83d605d846ff7e3ef6ab02009abfa0401e
cheny409 <cheny409@newschool.edu>
2023-12-10 20:15:51 -0500

    Update content.js

--- Visual Timer/scripts/content.js
+++ Visual Timer/scripts/content.js
@@ -1,16 +1,17 @@
 const badge = document.createElement('button');
 badge.style.position = 'fixed';
-badge.style.bottom = '20px';
-badge.style.right = '20px';
+badge.style.bottom = '50px';
+badge.style.right = '50px';
 badge.style.zIndex = '10000';
-badge.innerText = 'Click me';
+badge.style.fontSize = '50px';
+badge.innerText = '🔲';
 
 badge.addEventListener('click', () => {
   const container = document.createElement('div');
   container.style.position = 'relative';
   container.style.width = '100vw';
   container.style.height = '100vh';
-  container.style.zIndex = '101';// Ensure it appears above other content
+  container.style.zIndex = '101'; // Ensure it appears above other content
 
   function getRandomColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -20,7 +21,7 @@ badge.addEventListener('click', () => {
   parallelogram.style.backgroundColor = getRandomColor();
   parallelogram.style.width = '500px';
   parallelogram.style.height = '700px';
-  parallelogram.style.clipPath = 'polygon(0 90%, 0 10%, 100% 0, 100% 100%)';
+  parallelogram.style.clipPath = 'polygon(0 90%, 0 10%, 100% 0, 100% 100%)'; //CSS clip-path maker. https://bennettfeely.com/clippy/
   parallelogram.style.position = 'fixed';
 
   // Set position to top right
@@ -55,15 +56,6 @@ badge.addEventListener('click', () => {
         positionX += 1 * directionX; // Adjust the speed by changing the multiplier
         positionY += 1 * directionY;
 
-        // Change direction when reaching the container edges
-        if (positionX >= containerWidth || positionX <= 0) {
-            directionX = -initialDirectionX; // Reverse direction
-        }
-
-        if (positionY >= containerHeight || positionY <= 0) {
-            directionY = -initialDirectionY; // Reverse direction
-        }
-
         parallelogram.style.left = positionX + 'px';
         parallelogram.style.top = positionY + 'px';
 
@@ -90,17 +82,16 @@ badge.addEventListener('click', () => {
 // Create another new shape
 var shape = document.createElement('div');
 shape.style.backgroundColor = getRandomColor();
-shape.style.clipPath = 'polygon(0 76%, 18% 71%, 100% 94%, 85% 100%)';
+shape.style.clipPath = 'polygon(0 76%, 18% 71%, 100% 94%, 85% 100%)'; //CSS clip-path maker. https://bennettfeely.com/clippy/
 shape.style.width = '800px';
 shape.style.height = '500px';
 shape.style.position = 'fixed';
 
 // Set initial position at the left bottom
-var initialLeft = 0;
-var initialBottom = 0; // Adjusted for the bottom
-
+var initialLeft = 20;
+var initialBottom = 50; // Adjusted for the bottom
 shape.style.left = initialLeft + 'px';
-shape.style.bottom = initialBottom + '10px'; // Adjusted for the bottom
+shape.style.bottom = initialBottom + 'px';
 
 // Append the shape to the same container
 container.appendChild(shape);
@@ -109,9 +100,11 @@ animateShape(shape);
 
 function animateShape(shape) {
   var positionX = parseInt(shape.style.left);
-  var initialDirectionX = 1;
-  var directionX = initialDirectionX;
-  var containerWidth = window.innerWidth;
+  var positionY = parseInt(shape.style.top);
+  var twoinitialDirectionX = 1;
+  var twoinitialDirectionY = -1; // Adjusted for upward movement
+  var directionX = twoinitialDirectionX;
+  var directionY = twoinitialDirectionY;
   var animationDuration = 30000;
 
   var startTime = performance.now();
@@ -123,15 +116,12 @@ function animateShape(shape) {
           return;
       }
 
-      // Update the position (move horizontally)
+      // Update the positions (move diagonally)
       positionX += 1 * directionX;
-
-      // Change direction when reaching the container edges
-      if (positionX >= containerWidth || positionX <= 0) {
-          directionX = -directionX; // Reverse direction
-      }
+      positionY += 1 * directionY;
 
       shape.style.left = positionX + 'px';
+      shape.style.top = positionY + 'px';
 
       // Decrease the size of the shape
       var currentWidth = parseInt(shape.style.width);
@@ -141,7 +131,8 @@ function animateShape(shape) {
   }, 50); // Increase the interval time for a slower movement
 }
 
-  // put web contexts to the same container with shapes
+
+  // put the web context into the same container with shapes
   let els = Array.from(document.body.children).filter(el => el !== parallelogram);
  els.forEach((el) => {
    el.style.mixBlendMode = 'difference';
@@ -155,6 +146,10 @@ document.documentElement.appendChild(badge);
 
 function animateParallelogram(parallelogram) {
 }
+
+
+
+
 
 
 
