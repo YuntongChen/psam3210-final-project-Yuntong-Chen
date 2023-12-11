d78fb84e5395b3b85b6ba668472a8d60ae383c44
cheny409 <cheny409@newschool.edu>
2023-12-10 19:06:15 -0500

    Delete popup.js

--- popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('badge').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'createParallelogram' });
-  });
-});
